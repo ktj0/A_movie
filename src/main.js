3d37a41845ec19d09f29e3cf1b41f8b1611a76b7
Kim Taejin <rlaxowls0504@gmail.com>
2023-06-05 20:02:55 +0900

    movie_team_project
    영화카드 화면에 띄우기, 검색기능까지 import export를 하여서 코드를
    간략하게 리팩토링 하였습니다.

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { postingMovie } from "./movie_posting.js";
+import { searcinghMovie } from "./search.js";
+
+postingMovie();
+
+const $searchForm = document.querySelector('#search-form');
+const $searchInput = document.querySelector('#search-input');
+
+$searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    searcinghMovie($searchInput);
+})
+
+const $cardList = document.querySelector('#card-list');
+
+$cardList.addEventListener('click', event => {
+    if (event.target === $cardList) {
+        return;
+    }
+    if (event.target.matches('.movie-card')) {
+        window.open(`newPage.html?id=${event.target.id}`);
+        
+    } else {
+        window.open(`newPage.html?id=${event.target.parentNode.id}`);
+    }
+});
---
+++ src/movie_posting.js
@@ -0,0 +1,27 @@
+export const postingMovie = async () => {
+    const moviesInfo = await respondApi('ko-KR');
+    const $cardList = document.querySelector('#card-list');
+
+    $cardList.innerHTML = moviesInfo.map(movie => 
+        `<li class="movie-card" id="${movie['id']}">
+            <img src="https://image.tmdb.org/t/p/w200/${movie['poster_path']}">
+            <p class="movie-title">${movie['title']}</p>
+            <span style="display:none">${movie['overview']}/${'vote_average'}</span>
+        </li>`
+    ).join('');
+};
+
+async function respondApi(language) {
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZGRkOWQyYWVhMzc4ZTA4NTVhZjM3YzQzMDBiMTcxYiIsInN1YiI6IjY0NzM0NDkwYTE5OWE2MDBkYzRjYjk3YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._SbUcrThmlbJYXmIZfzbzJZmIUVqhuFVIoXK5mJmMHw'
+        }
+    };
+
+    const response = await fetch(`https://api.themoviedb.org/3/movie/top_rated?language=${language}&page=1`, options).then(response => response.json());
+    const moviesinfo = response['results'];
+
+    return moviesinfo;
+}

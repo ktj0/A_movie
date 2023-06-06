05626d1ba8165e2e3c16f36ca66cc118186835f5
Kim Taejin <rlaxowls0504@gmail.com>
2023-06-07 05:26:30 +0900

    Add comment
    코멘트 기능 추가
    해당 영화에 코멘트를 달 수 있게 했다.
    코멘트를 입력하려면 아이디와, 비밀번호를 입력한 후 할 수 있다.
    코멘트 삭제와 수정 기능은 아직 없다.

---
+++ src/comment.js
@@ -0,0 +1,89 @@
+const $txt = document.querySelector('#txt');
+const $form = document.querySelector('#form');
+const $reset = document.querySelector('#reset');
+const $id = document.querySelector('#id');
+const $pwd = document.querySelector('#pwd');
+const $allReset = document.querySelector('#all-reset');
+const $form2 = document.querySelector('#form2');
+const $cmt = document.querySelector('#cmt');
+
+const url = new URL(location.href);
+const idParams = +url.searchParams.get('id');
+
+export const comment = async () => {
+    $reset.addEventListener('click', () => {
+        localStorage.removeItem('id');
+        localStorage.removeItem('pwd');
+
+        window.location.reload();
+    });;
+
+    $allReset.addEventListener('click', () => {
+        localStorage.clear();
+    });
+
+    $form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        localStorage.setItem('id', $id.value);
+        localStorage.setItem('pwd', $pwd.value);
+
+        $id.value = '';
+        $pwd.value = '';
+        $form.style.display = 'none';
+    });
+
+    $txt.addEventListener('click', () => {
+        if (!localStorage.getItem('id')) {
+            $form.style.display = 'block';
+        } else {
+            $txt.removeAttribute('readonly');
+        }
+    });
+
+    $form2.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const cmtSto = Object.keys(localStorage).filter(item => !isNaN(item)).sort((a, b) => b - a);
+        let priKey = 0;
+
+        if (!cmtSto.includes('1')) {
+            priKey = 1;
+        } else {
+            priKey = +cmtSto[0] + 1;
+        }
+
+        const obj = {
+            id: localStorage.getItem('id'),
+            cmt: $txt.value,
+            movieId: idParams
+        };
+
+        localStorage.setItem(priKey, JSON.stringify(obj));
+
+        window.location.reload();
+    });
+}
+
+export function PostingCmt() {
+    const cmtSto = Object.keys(localStorage).filter(item => !isNaN(item)).sort((a, b) => b - a);
+    
+    if (!localStorage['id']) {
+        $reset.style.display = 'none';
+    } else {
+        $reset.style.display = 'blaock';
+    }
+
+    $cmt.innerHTML = cmtSto.map(item => {
+        const id = JSON.parse(localStorage.getItem(item))['id'];
+        const cmt = JSON.parse(localStorage.getItem(item))['cmt'];
+        const movieId = JSON.parse(localStorage.getItem(item))['movieId'];
+
+        if (movieId === idParams) {
+            return `<li class="cmt-li">
+                        <p id="cmt-id">${id}</p>
+                        <p id="cmt-review">${cmt}</p>
+                    </li>`
+        }
+    }).join('');
+}
--- src/newPage.js
+++ src/newPage.js
@@ -1,18 +1,28 @@
 import { respondApi } from "./movie_posting.js";
+import { comment } from "./comment.js";
+import { PostingCmt } from "./comment.js";
 
 const url = new URL(location.href);
 const idParams = +url.searchParams.get('id');
 const $div = document.querySelector('#div');
 
-const movieInfo = async () => {
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    movieInfo();
+    PostingCmt();
+    comment();
+})
+
+async function movieInfo() {
     const moviesInfo = await respondApi('ko-KR');
 
-    const selectMovie = moviesInfo.find(movie =>  movie['id'] === idParams);
+    const selectMovie = moviesInfo.find(movie => movie['id'] === idParams);
 
-    $div.innerHTML = `<img src="https://image.tmdb.org/t/p/w200/${selectMovie['poster_path']}" alt="" id="img">
+    $div.innerHTML = `<img src="https://image.tmdb.org/t/p/w200/${selectMovie['poster_path']}" id="img">
                     <p id="title">${selectMovie['title']}</p>
                     <p id="overview">${selectMovie['overview']}</p>
-                    <p id="vote-avg">${selectMovie['vote_average']}</p>`
-}
+                    <p id="vote-avg">${selectMovie['vote_average']}</p>`;
 
-movieInfo();
+
+};

0f7cde1ccda53e331c13e2a736e9ca9340644bed
baechanyong1 <110149885+baechanyong1@users.noreply.github.com>
2023-06-07 11:37:13 +0900

    card, release_date

--- src/newPage.js
+++ src/newPage.js
@@ -1,28 +1,31 @@
-import { respondApi } from "./movie_posting.js";
-import { comment } from "./comment.js";
-import { PostingCmt } from "./comment.js";
+import {respondApi} from './movie_posting.js';
+import {comment} from './comment.js';
+import {PostingCmt} from './comment.js';
 
 const url = new URL(location.href);
 const idParams = +url.searchParams.get('id');
 const $div = document.querySelector('#div');
 
-
-
 document.addEventListener('DOMContentLoaded', () => {
     movieInfo();
     PostingCmt();
     comment();
-})
+});
 
 async function movieInfo() {
     const moviesInfo = await respondApi('ko-KR');
 
     const selectMovie = moviesInfo.find(movie => movie['id'] === idParams);
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.onclick = function () {
+        alert(`영화 ID : ${idParams}`);
+    };
 
-    $div.innerHTML = `<img src="https://image.tmdb.org/t/p/w200/${selectMovie['poster_path']}" id="img">
+    card.innerHTML = `<img src="https://image.tmdb.org/t/p/w200/${selectMovie['poster_path']}" id="img">
                     <p id="title">${selectMovie['title']}</p>
                     <p id="overview">${selectMovie['overview']}</p>
-                    <p id="vote-avg">${selectMovie['vote_average']}</p>`;
-
-
-};
+                    <p id="vote-avg">${selectMovie['vote_average']}</p>
+                    <p id="release-date">${selectMovie['release_date']}</p>`;
+    $div.appendChild(card);
+}

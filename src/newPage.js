3d3be3cc83f7b20562e9386e5d75c212347d6ffa
Kim Taejin <rlaxowls0504@gmail.com>
2023-06-08 09:36:11 +0900

    Add movieID alert
    상세페이지에서 영화포스트를 클릭 시 해당 영화의 ID값이 alert창으로 출력

--- src/newPage.js
+++ src/newPage.js
@@ -17,6 +17,14 @@ $h1.addEventListener('click', () => {
     history.go(-1);
 });
 
+// console.log($div);
+
+$div.addEventListener('click', e => {
+    if (e.target.id === 'img') {
+        alert(`영화 ID: ${idParams}`);
+    }
+});
+
 async function movieInfo() {
     const moviesInfo = await respondApi('ko-KR');
 

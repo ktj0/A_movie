82d905c9a603cd55125938e8a709b83a9dc87d18
Kim Taejin <rlaxowls0504@gmail.com>
2023-06-08 11:14:37 +0900

    Add alert
    아이디와 비밀번호, 댓글 입력란에 아무 값도 입력하지 않았다면
    alert창이 출력된다.

--- src/comment.js
+++ src/comment.js
@@ -25,12 +25,22 @@ export const comment = async () => {
     $form.addEventListener('submit', e => {
         e.preventDefault();
 
-        localStorage.setItem('id', $id.value);
-        localStorage.setItem('pwd', $pwd.value);
+        if (!$id.value && !$pwd.value) {
+            // 비어있다면 alert창 출력
+            alert('ID와 PW를 입력해주세요.');
+        } else if (!$id.value) {
+            alert('ID를 입력해주세요.');
+        } else if (!$pwd.value) {
+            alert('PW를 입력해주세요.');
+        } else {
+            // ID와 PW값이 '' 비어있지않다면 setItem
+            localStorage.setItem('id', $id.value);
+            localStorage.setItem('pwd', $pwd.value);
 
-        $id.value = '';
-        $pwd.value = '';
-        $form.style.display = 'none';
+            $id.value = '';
+            $pwd.value = '';
+            $form.style.display = 'none';
+        }
     });
 
     $txt.addEventListener('click', () => {
@@ -44,27 +54,32 @@ export const comment = async () => {
     $form2.addEventListener('submit', e => {
         e.preventDefault();
 
-        const cmtSto = Object.keys(localStorage)
-            .filter(item => !isNaN(item))
-            .sort((a, b) => b - a);
-        let priKey = 0;
-
-        if (!cmtSto.includes('1')) {
-            priKey = 1;
+        if (!$txt.value) {
+            // 비어있다면 alert창 출력
+            alert('댓글을 입력해주세요');
         } else {
-            priKey = +cmtSto[0] + 1;
+            const cmtSto = Object.keys(localStorage)
+                .filter(item => !isNaN(item))
+                .sort((a, b) => b - a);
+            let priKey = 0;
+
+            if (!cmtSto.includes('1')) {
+                priKey = 1;
+            } else {
+                priKey = +cmtSto[0] + 1;
+            }
+
+            const obj = {
+                priKey: priKey,
+                userId: localStorage.getItem('id'),
+                cmt: $txt.value,
+                movieId: idParams
+            };
+            // 코멘트 입력 칸이 비어있지 않다면 setItem
+            localStorage.setItem(priKey, JSON.stringify(obj));
+
+            window.location.reload();
         }
-
-        const obj = {
-            priKey: priKey,
-            userId: localStorage.getItem('id'),
-            cmt: $txt.value,
-            movieId: idParams
-        };
-
-        localStorage.setItem(priKey, JSON.stringify(obj));
-
-        window.location.reload();
     });
 };
 
